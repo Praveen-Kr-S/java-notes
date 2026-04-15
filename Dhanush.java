0b8dfb5758f6024afc101c24eb38cf3aa60531cd
PraveenKumar S <praveenkrsslm69@gmail.com>
2026-04-15 13:33:42 +0530

    Add files via upload

---
+++ Dhanush.java
@@ -0,0 +1,138 @@
+package Kioshore;
+
+public class Dhanush {
+
+	public static void main(String[] args) {
+//		int a=20;
+//		int b=10;
+//		int c=10;
+//		int total=a+b+c;
+//		System.out.println(total/3);
+//		float pi=3.14f;
+//		int radius =25;
+//		System.out.println(pi*Math.pow(radius,2));
+//		int a=5;
+//		int b=5;
+//		System.out.println(a*b);
+		
+// Logical operator -> &&, ||, !()
+//		int a = 10;
+//		int b = 30;
+//		int c = 40;
+//		int d = 30;
+//		System.out.println(a != b && a <= d && c > a);
+//		System.out.println(a == b || a == d || c != a);
+//		System.out.println(!(false));
+		
+//		control statements/flow of control
+//		2.conditional statements
+		
+//		1.if statement
+		
+//		int num = 8;
+//		if (num == 5) {
+//			System.out.println("Num is 5");
+//		}
+//		else {
+//			System.out.println("Num is not a 5");
+//		}
+		
+//		2.odd or even
+		
+//		int num = 7;
+//		if (num%2 == 0) {
+//			System.out.println("Num is Even Number");
+//		}
+//		else {
+//			System.out.println("Num is Odd Number");
+//		}
+		
+		
+		
+//		else-if
+		
+//		31 days -> 1,3,5,7,9,11
+//		28 -> 2
+//		int m = 8;
+//		if(m==1 || m==3 || m==5|| m==7|| m==9|| m==11) {
+//			System.out.println("31 Days");
+//		}
+//		else if(m == 2) {
+//			System.out.println("28 Days");
+//		}
+//		else {
+//			System.out.println("30 Days");
+//		}
+		
+		
+		
+	
+//		Nested if condition
+		
+		/*
+		 * syntax of Nested if
+		 * 
+		 * if(condition){
+		 * 	//true statement
+		 * 	if(condition){
+		 * 		//true statement
+		 * 		}
+		 * 	else{
+		 * 		//false statemt
+		 * 		}
+		 * }
+		 * else{
+		 * 	//false statement
+		 * }
+		 * 
+		 */
+		
+		
+//		1 email validation
+		
+//		String email = "subash@gmail.com";
+//		if(email == "subash@gmail.com") {
+//			System.out.println("Email is Valid😊...");
+//			String pass = "a1234";
+//			if(pass == "a123") {
+//				System.out.println("Login Success😁😁");
+//			}
+//			else {
+//				System.out.println("Invalid Password 🙄");
+//			}
+//		}
+//		else {
+//			System.out.println("Invalid Email😒...");
+//		}
+		
+		
+//		switch-case
+		
+		/*
+		 * switch(expression){
+		 * 	case value: //statement; break;
+		 * 	case value: //statement; break;
+		 * 	case value: //statement; break;
+		 * 	default: //statement;
+		 * }
+		 */
+//		find super hero powers
+		
+		byte position = 2;
+		switch(position) {
+			case 1: System.out.println("Choota bheem..."); break;
+			case 2: System.out.println("Kirmada..."); break;
+			case 3: System.out.println("Chutki..."); break;
+			case 4: System.out.println("Indra varma..."); break;
+			default: System.out.println("Invaild Number, give only 1 to 4..");
+		}
+		
+		
+		
+		
+		
+       
+
+	}
+
+}
